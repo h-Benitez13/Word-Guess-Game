1866d35f3d67e256acc19e425aff5613d5d8d863
h-Benitez13 <hbenitez3711@gmail.com>
2018-11-18 21:07:24 -0500

    we did a whole 360 today, but hard work pays off...i hope

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,19 +1,71 @@
-//first off, we need a word array
-//for zee guesses
-//and a topic: mcdonalds menu
-//create all the variables for the game 
-//letters array to be chosen for each word
-//answer array where the user guesses the letter
-//letters that have been selected array that are not in the word
+var wordBank = ["nuggets", "fries", "cheeseburger", "mcflurry", "soda"]
+var wins = 0;
+var losses = 0;
+var wrongLetter = [];
+var guessesLeft = 9;
+var underScores = [];
+var userGuesses = [];
+var randWord = [];
+// function
+// ==================
+function startGame() {
+    // picks random word
+    randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    console.log("random word =" + randWord);
+    for (var i = 0; i < randWord.length; i++) {
+        underScores.push("_ ");
 
+    }
+    // printing underScores to the screen
 
-var words =     ["nuggets", "fries", "cheeseburger", "mcflurry", "soda"]
-let letter = 'abcdefghijklmnopqrstuvwxyz'.split(" ");
-var asnwerArray= [];
-var wrongGuess= [];
+    document.getElementById("visualWord").textContent = underScores.join("");
+    console.log(underScores);
 
+    // reset whenever we start the game
+    wrongLetter = [];
+    guessesLeft = 9;
 
-//next want to randomize the words
-//jquery lesson with Math (:
+    // PRINT ON THE SCREEN
+    document.getElementById("guesses-left").textContent = guessesLeft;
+    document.getElementById("wrongLetter").textContent = "Letters guessed: " + wrongLetter;
 
-var random= Math.floor(Math.random()*(words.length));
+}
+
+// capture the users input
+document.onkeyup = function (event) {
+    userGuesses = event.key;
+
+    // logiccccc
+    // if the letter pressed is within the array word
+    //  
+    if (randWord.indexOf(userGuesses) > -1) {
+        // if the letter exist inside the word
+        // then put in the underscore
+        for (var i = 0; i < randWord.length; i++) {
+            if (randWord[i] === userGuesses) {
+                underScores[i] = userGuesses;
+                console.log(underScores);
+                
+                
+            }
+        }
+    }
+    // if the letter doesnt exist then you push towards the wrong letter array
+    else if (randWord[i] ===  randWord[i]) {
+        wrongLetter.push(userGuesses);
+        guessesLeft--;
+
+        console.log(wrongLetter);
+        console.log(guessesLeft);
+    }
+    function winLose(){
+        if (guessesLeft === 0){
+            alert("loser");
+        }
+    }
+    
+}
+// Main
+// ==========================================
+
+startGame();
